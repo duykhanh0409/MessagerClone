d92e9c64cb0dbabb908456dd885f9bb38a6689e1
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2024-01-20 14:51:05 +0700

    Initial Commit

---
+++ MessagerClone/App/MessagerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagerCloneApp.swift
+//  MessagerClone
+//
+//  Created by Khanh Nguyen on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessagerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MessagerClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessagerClone
+//
+//  Created by Khanh Nguyen on 20/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
