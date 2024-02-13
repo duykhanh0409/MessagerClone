290c2192a28de0fc297f9d88a54442a17cff52a3
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2024-02-13 13:45:10 +0700

    implement add user information to firebase firestore

--- MessagerClone/Authentication/Service/AuthService.swift
+++ MessagerClone/Authentication/Service/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 class AuthService {
     
@@ -16,9 +17,13 @@ class AuthService {
     
     init(){
         self.userSession = Auth.auth().currentUser // this function to help store user login in the app
+        Task {
+            try await UserService.shared.fetchCurrentUser()
+        }
         print("DEBUG: USer session id is \(userSession?.uid)")
     }
     
+    @MainActor // do this action on main thread
     func login(withEmail email: String, password:String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -28,10 +33,12 @@ class AuthService {
         }
     }
     
+    @MainActor
     func createUser(withEmail email: String, password:String, fullname: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            try await self.uploadUserData(email: email, fullname: fullname, id: result.user.uid)
             print("DEBUG: CREARE USER \(result.user.uid)")
         }catch {
             print("DEBUG FAIL TO CREARE USER \(error.localizedDescription)") // question where is error com from, answer Swift very cool, can auto give access to this error in site catch, don't need declare error
@@ -45,5 +52,11 @@ class AuthService {
         }catch {
             print("DEBUG: fail to signOut \(error.localizedDescription)")
         }
+    }
+    
+    private func uploadUserData(email: String, fullname: String, id: String) async throws {
+        let user = User(fullname: fullname, email: email, profileImageUrl: nil)
+        guard let encodeUser = try? Firestore.Encoder().encode(user) else {return}
+        try await Firestore.firestore().collection("users").document(id).setData(encodeUser)
     }
 }
--- MessagerClone/Service/UserService.swift
+++ MessagerClone/Service/UserService.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import Firebase
+
+class UserService {
+    
+    @Published var currentUser: User?
+    
+    static let shared = UserService()
+    
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshot.data(as: User.self) //decode
+        self.currentUser = user
+        print("DEBUG: current user in sevice is \(currentUser)")
+    }
+}
